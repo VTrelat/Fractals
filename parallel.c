69733eaf334c57e5c545a68418eb3b93146ebef6
Vincent Trélat <vincent.trelat@free.fr>
2023-03-31 10:26:41 +0200

    Added flags for user input using `getopt`

--- parallel.c
+++ parallel.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <unistd.h>
 
 #define MAX_ITERATIONS 1000
 
@@ -25,10 +26,10 @@ unsigned int iterate(long double cx, long double cy, int max_iterations, long do
     while (iterations < max_iterations)
     {
         x1 = x0 * x0 - y0 * y0 + cx;
-        y1 = 2 * fabsl(x0 * y0) + cy;
+        y1 = 2 * x0 * y0 + cy;
         if (x1 * x1 + y1 * y1 > clipping * clipping)
         {
-            break;
+            return iterations;
         }
         x0 = x1;
         y0 = y1;
@@ -105,75 +106,89 @@ unsigned char *paste_top_bottom(unsigned char *im1, unsigned char *im2, int widt
 
 int main(int argc, char *argv[])
 {
-    long double zoom;
-    long double c0x;
-    long double c0y;
-    int width;
-    int height;
-    long double clipping;
-    if (argc == 7)
+    long double zoom = 1.0;
+    long double c0x = 0.0;
+    long double c0y = 0.0;
+    int width = 1000;
+    int height = 1000;
+    long double clipping = 2.0;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "z:x:y:w:h:c:")) != -1)
     {
-        zoom = atof(argv[1]);
-        c0x = atof(argv[2]);
-        c0y = atof(argv[3]);
-        width = atoi(argv[4]);
-        height = atoi(argv[5]);
-        clipping = atof(argv[6]);
+        switch (opt)
+        {
+        case 'z':
+            zoom = atof(optarg);
+            break;
+        case 'x':
+            c0x = atof(optarg);
+            break;
+        case 'y':
+            c0y = atof(optarg);
+            break;
+        case 'w':
+            width = atoi(optarg);
+            break;
+        case 'h':
+            height = atoi(optarg);
+            break;
+        case 'c':
+            clipping = atof(optarg);
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-z zoom] [-x c0x] [-y c0y] [-w width] [-h height] [-c clipping]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
 
-        // parallel
+    // parallel
+    // measure time
+    double start = omp_get_wtime();
 
-        // measure time
-        double start = omp_get_wtime();
-
-        unsigned char *im[16];
+    unsigned char *im[16];
 #pragma omp parallel for
-        for (int i = 0; i < 16; i++)
-        {
-            int x_offset = (i % 4) * width / 4 - width / 2;
-            int y_offset = (i / 4) * height / 4 - height / 2;
-            im[i] = compute_set(zoom, c0x, c0y, width / 4, height / 4, MAX_ITERATIONS, clipping, x_offset, y_offset, i);
-        }
-#pragma endregion
-        double end = omp_get_wtime();
-        printf("\nTime: %.2f s\n", end - start);
-        unsigned char *im2[8];
-#pragma omp for
-        for (int i = 0; i < 4; i++)
-        {
-            im2[2 * i] = paste_left_right(im[4 * i], im[4 * i + 1], width / 4, height / 4);
-            im2[2 * i + 1] = paste_left_right(im[4 * i + 2], im[4 * i + 3], width / 4, height / 4);
-        }
-        unsigned char *im3[4];
-#pragma endregion
-
-#pragma omp for
-        for (int i = 0; i < 4; i++)
-        {
-            im3[i] = paste_left_right(im2[2 * i], im2[2 * i + 1], width / 2, height / 4);
-        }
-        unsigned char *im4[2];
-#pragma endregion
-
-#pragma omp for
-        for (int i = 0; i < 2; i++)
-        {
-            im4[i] = paste_top_bottom(im3[2 * i], im3[2 * i + 1], width, height / 4);
-        }
-        save_ppm(paste_top_bottom(im4[0], im4[1], width, height / 2), width, height, "out.ppm");
-#pragma endregion
-
-        // sequential (for comparison)
-        // start = omp_get_wtime();
-        // unsigned char *im_seq = compute_set(4 * zoom, c0x, c0y, width, height, MAX_ITERATIONS, clipping, -width / 2, -height / 2, 0); // 4x zoom to match parallel version
-        // end = omp_get_wtime();
-        // printf("\nTime: %.2f s\n", end - start);
-        // save_ppm(im_seq, width, height, "out2.ppm");
-
-        return 0;
-    }
-    else
+    for (int i = 0; i < 16; i++)
     {
-        printf("Usage: %s <zoom> <c0x> <c0y> <width> <height> <clipping>\n", argv[0]);
-        return 1;
+        int x_offset = (i % 4) * width / 4 - width / 2;
+        int y_offset = (i / 4) * height / 4 - height / 2;
+        im[i] = compute_set(zoom, c0x, c0y, width / 4, height / 4, MAX_ITERATIONS, clipping, x_offset, y_offset, i);
     }
+#pragma endregion
+    double end = omp_get_wtime();
+    printf("\nTime: %.2f s\n", end - start);
+    unsigned char *im2[8];
+#pragma omp for
+    for (int i = 0; i < 4; i++)
+    {
+        im2[2 * i] = paste_left_right(im[4 * i], im[4 * i + 1], width / 4, height / 4);
+        im2[2 * i + 1] = paste_left_right(im[4 * i + 2], im[4 * i + 3], width / 4, height / 4);
+    }
+    unsigned char *im3[4];
+#pragma endregion
+
+#pragma omp for
+    for (int i = 0; i < 4; i++)
+    {
+        im3[i] = paste_left_right(im2[2 * i], im2[2 * i + 1], width / 2, height / 4);
+    }
+    unsigned char *im4[2];
+#pragma endregion
+
+#pragma omp for
+    for (int i = 0; i < 2; i++)
+    {
+        im4[i] = paste_top_bottom(im3[2 * i], im3[2 * i + 1], width, height / 4);
+    }
+    save_ppm(paste_top_bottom(im4[0], im4[1], width, height / 2), width, height, "out.ppm");
+#pragma endregion
+
+    // sequential (for comparison)
+    // start = omp_get_wtime();
+    // unsigned char *im_seq = compute_set(4 * zoom, c0x, c0y, width, height, MAX_ITERATIONS, clipping, -width / 2, -height / 2, 0); // 4x zoom to match parallel version
+    // end = omp_get_wtime();
+    // printf("\nTime: %.2f s\n", end - start);
+    // save_ppm(im_seq, width, height, "out2.ppm");
+
+    return 0;
 }
