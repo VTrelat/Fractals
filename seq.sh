c0b2d1c6991edb7bd16fca1f8f04033730069225
Vincent Trélat <vincent.trelat@free.fr>
2023-04-01 19:00:05 +0200

    Update seq.sh

--- seq.sh
+++ seq.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-N=800
-z=8e-16
+N=1000
+z=0.00000000000008
+x=-1.940967706102894
+y=0.0010053830006
 for i in $(seq 0 $N)
 do
     # result=$(echo "e(-$i/20)" | bc -l);
-    result=$(echo "1 - (1 - $z) / (1 + e(-10 * $i / (.2*$N)+.2*$N/10))" | bc -l);
+    result=$(echo "1 - (1 - $z) / (1 + e(-10 * $i / (0.2*$N)+ $N / 100))" | bc -l);
     echo "[$i/$N, $result]";
-    ./parallel -z $result -x -0.2262670179899671 -y 1.1161743290644637 -w 1920 -h 1080 -o seq/$i.ppm
+    ./parallel -z $result -x $x -y $y -w 1920 -h 1080 -o seq/$i.ppm
 done
