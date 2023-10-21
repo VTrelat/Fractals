0bb56cf81cb25d7074d6c7948fcb13b1bf6dfc23
Vincent Trélat <vincent.trelat@free.fr>
2023-10-21 16:01:35 +0200

    Update seq.sh

--- seq.sh
+++ seq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-N=100
+N=144000
 z=0.00000000001
 x=-1.255615900225
 y=0.03497725995
@@ -11,5 +11,5 @@ do
     result=$(echo "$result / $alpha" | bc -l);
     # result=$(echo "1 - (1 - $z) / (1 + e(-1 * $i / (0.2*$N)+ $N / 10))" | bc -l);
     echo "[$i/$N, $result]";
-    ./parallel -z $result -x $x -y $y -w 960 -h 540 -o seq/$i.ppm
+    ./parallel -z $result -x $x -y $y -w 3840 -h 2160 -o seq/$i.ppm
 done
